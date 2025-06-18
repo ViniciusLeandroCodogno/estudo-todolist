19bc1afc21b0d50650d31b898bf917701a9edc3a
Vinicius Codogno <vilecodo@gmail.com>
2025-06-18 11:06:58 -0300

    feature: add task field to Task model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import datetime
 
 class Task(BaseModel):
     name_task: str
     desc_task: str
+    category_task: str
+    date_task: datetime = Field(
+        default_factory=lambda: datetime.today()
+    )
