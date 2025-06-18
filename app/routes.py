e613e0f1434547510b1ccb1797750201ff512d6e
Vinicius Codogno <vilecodo@gmail.com>
2025-06-18 11:10:07 -0300

    feature: implement route to search task by category

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException
 from bson import ObjectId
+from typing import List
 from fastapi import APIRouter
 from app.database import collection
 from app.models import Task
@@ -11,6 +12,24 @@ def read_task():
     tasks = collection.find_one()
     return {"tasks": str(tasks)}
 
+@router.get("/tasks/{category_task}", response_model=List[Task])
+def get_tasks_by_category(category_task: str):
+    task_cursor = collection.find(
+        {"category_task": category_task},
+        {"_id": 1, "name_task": 1, "desc_task": 1, "category_task": 1, "date_task": 1}
+    )
+    tasks = [
+        {
+            "id": str(task["_id"]),
+            "name_task": task.get("name_task"),
+            "desc_task": task.get("desc_task"),
+            "category_task": task.get("category_task"),
+            "date_task": task.get("date_task")
+        }
+        for task in task_cursor
+    ]
+    return tasks
+    
 @router.post("/tasks")
 def create_task(task: Task):
     result = collection.insert_one(task.dict())
